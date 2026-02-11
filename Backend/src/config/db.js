119408bbaf28fbc6a805acda8396cdebd443b83a
Yamlak Negash <yamlaknegash96@gmail.com>
2026-02-11 13:57:10 +0300

    db wait-time increased and vercel.json added on frontend to cope with routing on SPA

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -5,7 +5,9 @@ import logger from "../utils/logger.js";
 const connectDB = async () => {
   try {
     mongoose.set("strictQuery", true);
-    await mongoose.connect(appConfig.mongoUri);
+    await mongoose.connect(appConfig.mongoUri, {
+      serverSelectionTimeoutMS: 20000,
+    });
     logger.info("MongoDB connected ✅");
   } catch (error) {
     logger.error(`MongoDB connection error: ${error.message}`);
